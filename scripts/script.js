d86b128fa96f7228033abedbfd39e0268dd4b919
Alan Pruitt <alanpruitt@gmail.com>
2022-03-02 18:59:34 -0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+alert("Welcome to my site! This is Alan Pruitt's VIT Final Project Page.")
+
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
